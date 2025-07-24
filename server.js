c9e6af689dffc05c8f5b3bd40beacc28bd474622
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-07-24 01:07:24 -0400

    send submissions to admin

--- server.js
+++ server.js
@@ -27,18 +27,20 @@ io.on('connection', (socket) => {
 
   socket.on('submit', (data) => {
     console.log('Received submission:', data.length);
+    io.to('beauty1').emit('submission', data);
+
     // io.to('beauty1').emit('submit', data);
   });
 
-  socket.on('approve', (data) => {
-    console.log('Approved data:', data);
-    io.to('beauty1').emit('approved', data);
-  });
+  // socket.on('approve', (data) => {
+  //   console.log('Approved data:', data);
+  //   io.to('beauty1').emit('approved', data);
+  // });
 
-  socket.on('replay-test', (data) => {
-    console.log('Replay test event received:', data);
-    io.to('beauty1').emit('replay-test', data); // forward it for testing
-  });
+  // socket.on('replay-test', (data) => {
+  //   console.log('Replay test event received:', data);
+  //   io.to('beauty1').emit('replay-test', data); // forward it for testing
+  // });
 
   socket.on('disconnect', () => {
     console.log('Client disconnected:', socket.id);
